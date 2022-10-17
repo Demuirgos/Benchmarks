834708bc7d920ca7b551802e3b2e98524c31f0b0
Ayman Bouchareb <Ayman.bouchareb@outlook.fr>
2022-10-17 14:33:16 +0100

    added default implementer generator

---
+++ generator/Attribute.cs
@@ -0,0 +1,10 @@
+namespace Attributes;
+public class ImplementAttribute : System.Attribute
+{
+    public ImplementAttribute() {}
+}
+
+public class DefaultAttribute : System.Attribute
+{
+    public DefaultAttribute(string description, string value) {}
+}
---
+++ target/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection.Metadata;
+using Attributes;
+namespace Program
+{
+    public interface ITest
+    {   
+        [Default("Description", "Value")]
+        public static string TestString { get; }
+
+        [Default("Description", "23")]
+        public static int TestInt { get; }
+        
+    }
+    class Source
+    {
+        public static void Main(string[] args) {
+        }
+    }
+}
