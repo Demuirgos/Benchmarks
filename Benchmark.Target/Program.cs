209a8fa302467ff4f36208eccdc005ad5d1aa816
Ayman Bouchareb <Ayman.bouchareb@outlook.fr>
2022-10-23 19:50:55 +0100

    Added benchmarks back

--- Benchmark.Target/Program.cs
+++ Benchmark.Target/Program.cs
@@ -1,40 +1,41 @@
-﻿using BenchmarkDotNet.Attributes;
+﻿using System.Diagnostics;
+using BenchmarkDotNet.Attributes;
 using Benchmarks.Interception;
 using Benchmarks.Models;
-using PostSharp.Aspects.Advices;
-using PostSharp.Extensibility;
-using System.Diagnostics;
-using System.Reflection;
-using static TestFunc;
 using BenchmarkDotNet.Running;
 
-var i = SumSync1(0, 100);
-var j = await SumAsync1(0, 100);
-Console.WriteLine($"Results are : [{i}, {j}]");
-
-//BenchmarkRunner.Run<TestFunc>();
+BenchmarkRunner.Run<TestFunc>();
 
 [MemoryDiagnoser]
 public class TestFunc {
     public IEnumerable<int> ValuesForlow => new int[] { 0, 100, 1000};
     public IEnumerable<int> ValuesForHigh => new int[] { 100, 1000, 1000000};
+    [ParamsSource(nameof(ValuesForlow))] public int lowBound { get; set; }
+    [ParamsSource(nameof(ValuesForHigh))] public int highBound { get; set; }
 
-    [ParamsSource(nameof(ValuesForlow))]
-    public int lowBound { get; set; }
-    [ParamsSource(nameof(ValuesForHigh))]
-    public int highBound { get; set; }
     [Benchmark] public int SumSyncAttr() => SumSync1(lowBound, highBound);
     [Benchmark] public async Task<int> SumAsyncAttr() => await SumAsync1(lowBound, highBound);
+    [Benchmark] public int SumSyncClass() => SumSync2(lowBound, highBound);
+    [Benchmark] public async Task<int> SumAsyncClass() => await SumAsync2(lowBound, highBound);
+
 
     [Monitor(InterceptionMode: InterceptionMode.ExecutionTime, LogDestination : LogDestination.Console)]
     public static int SumSync1(int n, int k)
+        => ActionSync(n, k);
+
+    [Monitor(InterceptionMode: InterceptionMode.ExecutionTime, LogDestination : LogDestination.Console)]
+    public static async Task<int> SumAsync1(int n, int k) 
+        => await ActionAsync(n, k);
+
+    public static int SumSync2(int n, int k)
     {
+        using DisposableTimer? timer = new DisposableTimer();
         return ActionSync(n, k);
     }
 
-    [Monitor(InterceptionMode: InterceptionMode.Failures, LogDestination : LogDestination.Console)]
-    public static async Task<int> SumAsync1(int n, int k)
+    public static async Task<int> SumAsync2(int n, int k)
     {
+        using DisposableTimer? timer = new DisposableTimer();
         return await ActionAsync(n, k);
     }
 
@@ -52,12 +53,15 @@ public class TestFunc {
         int res = 0;
         for (int i = n; i <= k; i++)
         {
-            if((new Random()).Next(0, 100) == 0)
-            {
-                throw new Exception("Random Exception");
-            }
             res += await Task.FromResult(i);
         }
         return res;
     }
+}
+
+public class DisposableTimer :IDisposable
+{
+    private Stopwatch watch = new();
+    public DisposableTimer() => watch.Start();
+    public void Dispose() => Console.WriteLine(watch.Elapsed.TotalMilliseconds);
 }
